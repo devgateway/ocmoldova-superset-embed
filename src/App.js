a2078e75f95c7a1d537ace5cef748d0bfdc30067
Mihai Postelnicu <mpostelnicu@gmail.com>
2023-06-29 13:47:46 +0300

    IREXCM-2 Use OpenDataServices/ocdsdata locally

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import "./App.css"
 async function fetchAccessToken() {
   try {
     const body = {
-      username: "",
-      password: "",
+      username: "tokencreator", //this user only creates guest tokens, has no other access. Therefore we can use it in the frontend
+      password: "tokencreator",
       provider: "db",
       refresh: true,
     }
@@ -33,52 +33,48 @@ async function fetchAccessToken() {
 async function fetchGuestToken() {
   const accessToken = await fetchAccessToken();
   try {
-    const payload = {
-      "user": {
-        "username": "guest",
-        "first_name": "Guest",
-        "last_name": "User"
-      },
-      "resources": [{
-        "type": "dashboard",
-        "id": "7"
-      }],
-      "rls": []
-    }
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${accessToken}`,
-    };
+      var data = {
+        "user": {
+          "username": "guest",
+          "first_name": "Guest",
+          "last_name": "Guest"
+        },
+        "resources": [{
+          "type": "dashboard",
+          "id": "14"
+        }],
+        "rls": []
+      };
 
-    const response = await fetch(
-      "http://localhost:8088/api/v1/security/guest_token",
-      {
-        method: "POST",
-        body: JSON.stringify(payload),
-        headers: headers
-      }
-    )
+    const response = await fetch("http://localhost:8088/api/v1/security/guest_token/", 
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${accessToken}`
+      },
+      body: JSON.stringify(data),
+    });
+
     const jsonResponse = await response.json()
     return jsonResponse?.token
   } catch (error) {
     console.error(error)
   }
-}
+};
 
 
 function App() {
-  const getToken = async () => {
-    const token = await fetchGuestToken();
-    return token;
-  }
+ 
 
   useEffect(() => {
     const embed = async () => {
+      var guestToken = await fetchGuestToken();
       await embedDashboard({
-        id: "f167fcca-0312-484c-8364-2a7a377c75d3", // given by the Superset embedding UI
+        id: "d025df9e-7e8b-4c9a-a8f4-8ec781c075fe", // given by the Superset embedding UI
         supersetDomain: "http://localhost:8088",
         mountPoint: document.getElementById("dashboard"), // html element in which iframe render
-        fetchGuestToken: () => "",
+        fetchGuestToken: () => guestToken,
         dashboardUiConfig: {
           hideTitle: true,
           hideChartControls: true,
